85c1f758f1233d76585c1bb28734db0db7c46048
Hiroki Kondo <kondo.matr02@gmail.com>
2020-07-07 20:11:37 +0900

    #3完了

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
