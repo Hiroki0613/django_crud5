df7c973791ac28c7b76705623889f2e7c5ca89c8
Hiroki Kondo <kondo.matr02@gmail.com>
2020-07-14 20:04:22 +0900

    #12完了

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+
 ]
