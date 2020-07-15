ec1cf687b9e02cf952b102bbeba9e8d66d298872
Hiroki Kondo <kondo.matr02@gmail.com>
2020-07-15 13:02:25 +0900

    #13完了

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import (
     DetailView,
     CreateView,
     UpdateView,
+    DeleteView,
 )
 from .models import Post
 
@@ -35,6 +36,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
